71e55b940ad017ff0e91beed1cb14f8cd2906bb7
Falrach94 <Samantha.Zehrer@gmail.com>
2025-04-25 14:48:38 +0200

    fixed invlaid substitution

--- bootstrap_server.sh
+++ bootstrap_server.sh
@@ -1,13 +1,6 @@
-#! /bin/sh
-
-#flask --debug --app ./app/index run
-#export FLASK_APP=./program/server/app/index.py
-#export PYTHONPATH="media/SSD_Data/Coding/Trader"
-#flask --debug run -h 0.0.0.0 -p 5000
+#! /bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-#echo $SCRIPT_DIR
 
 export PYTHONPATH="$SCRIPT_DIR/src"
 
